8f1499f8f9dff938794b24a4164bdc572a700b8c
Pawel Zakiewicz <pawel.zakiewicz@ft.com>
2017-11-27 13:24:37 +0000

    Settig-up

---
+++ parser.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import yaml, sys
+from jinja2 import Template
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-t", "--template", dest="templateFile",
+                  type="string", action="store",
+                  help="template file to parse")
+parser.add_option("-p", "--params", dest="paramsFile",
+                  type="string", action="store",
+                  help="YAML parameters file")
+parser.add_option("-s","--stage", dest="stage",
+                  type="string", action="store",
+                  help="parameters 'stage'")
+
+(options, noise) = parser.parse_args()
+
+if len(sys.argv) != 7 or len(noise) > 0:
+    parser.print_help()
+else:
+    with open(options.paramsFile) as parameters:
+        parameters = yaml.load(parameters)
+    with open(options.templateFile) as template:
+        template = Template(template.read())
+
+    print(template.render(parameters[options.stage],ENV=options.stage))
